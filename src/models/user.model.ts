eba6d837b5f3cd91ddbcbf3abb2a500aae2a15ea
Roshan Khadka <khadkaasd963@gmail.com>
2025-12-31 09:09:32 +0545

    adding user mongoose model for authentication

---
+++ src/models/user.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { UserType } from "../types/user.type";
+
+const userMongoSchema: Schema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export interface IUser extends UserType, Document {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const UserModel = mongoose.model<IUser>("User", userMongoSchema);
---
+++ src/types/user.type.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  fullName: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(6),
+  role: z.enum(["user", "admin"]).default("user"),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
+
+export type UserDocument = UserType & {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
